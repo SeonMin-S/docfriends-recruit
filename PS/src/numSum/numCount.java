bf891dfd06ebe14bf1c5c1eadfe4f2a5529acb99
seonmin <seonmin601@gmail.com>
2019-09-24 16:34:01 +0900

    1. 숫자배열

---
+++ PS/src/numSum/numCount.java
@@ -0,0 +1,15 @@
+package numSum;
+
+public class numCount {
+
+	public static void main(String[] args) {
+		var q = "fgdfgeg3445546sd023fr";
+		var num = q.replaceAll("[^0-9]","");
+		
+		System.out.println(num);
+		
+		
+	}
+
+}
+
