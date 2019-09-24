2fee38ffbf57e800641c2cdb82bf294d621a201d
seonmin <seonmin601@gmail.com>
2019-09-24 16:35:56 +0900

    2. a+b?

--- PS/src/numSum/numCount.java
+++ PS/src/numSum/numCount.java
@@ -6,9 +6,22 @@ public class numCount {
 		var q = "fgdfgeg3445546sd023fr";
 		var num = q.replaceAll("[^0-9]","");
 		
-		System.out.println(num);
+		String numbers[] = {};
+		int a = 10; //lowest
+		int b = 0; //highest
 		
+//		System.out.println(num);
 		
+		numbers = num.split("");
+		for(int i = 0; i<numbers.length;i++) {
+			int comp = Integer.parseInt(numbers[i]);
+			System.out.println(comp);
+			if(comp>b) b= comp;
+			if(comp<a) a= comp;
+		}
+//		System.out.println("b="+b);
+//		System.out.println("a="+a);
+		System.out.println(a+b);
 	}
 
 }
